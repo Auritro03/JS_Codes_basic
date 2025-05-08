2e7f02fe8f1c71d7fab0ff529a330520748d42c5
Easa Yeasfi Bin Islam <oritrobinislam@gmail.com>
2025-05-08 21:46:27 +0600

    basics

---
+++ array.js
@@ -0,0 +1,13 @@
+// var kaka = ["jah", "an, gir"]
+// // kaka.pop()
+// // // kaka.push("khala")
+
+// if (kaka.length > 0) {
+//     console.log(kaka[0])
+// }
+
+const chacha = [122, 322, 432, 76, 342, 856]
+
+ chacha [2] = 01
+
+console.log(chacha)
