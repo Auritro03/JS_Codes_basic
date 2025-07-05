a2f651580143f545742d752acbfa85e26f295863
Auritro <oritrobinislam@gmail.com>
2025-07-05 22:48:29 +0600

    ForEach In JavaScript

--- foreach.js
+++ foreach.js
@@ -1,3 +1,6 @@
+// This method do every work but never return anything. In JavaScript forEach is written with the capital 'E'
+
+
 const Number = [1, 2, 43, 56, 6, 67]
 const result = Number.forEach(n=> n*2)
 console.log(result)
