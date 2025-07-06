1ced06843ac69e193e63d7675de7ce54a16fb181
Auritro <oritrobinislam@gmail.com>
2025-07-06 22:27:03 +0600

    Filter in JavaScript

---
+++ filter.js
@@ -0,0 +1,4 @@
+// Filter is basically filter... HaHa. You know what filters do right? Same thing jsut coding version of JS.. If you donno then google it.
+const number = [1, 20, 30, 40]
+const result = number.filter( x => x <  20 )
+console.log (result)
